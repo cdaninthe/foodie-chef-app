98804ac8f5cae00123e567e2329b3c9229c7d6d9
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-05-15 20:20:31 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  resources :reviews
+  resources :recipes 
+  resources :users, only: [:index]
+  
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/me', to: 'users#show'
+  post '/signup', to: 'users#create'
+
+  get '/recipes/:id/reviews', to: 'recipes#reviews'
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
